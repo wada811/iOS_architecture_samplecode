a07fd28d9d137a32cf37f6108f5b42a30e603320
wada811 <at.wada811@gmail.com>
2019-11-28 10:59:40 +0900

    Move logic to Model

--- 07/RxSimpleSample/RxSimpleSample/ViewModel.swift
+++ 07/RxSimpleSample/RxSimpleSample/ViewModel.swift
@@ -12,57 +12,47 @@ import RxSwift
 final class ViewModel {
     let validationText: Observable<String>
     let loadLabelColor: Observable<UIColor>
+    let disposeBag = DisposeBag()
 
     init(idTextObservable: Observable<String?>,
          passwordTextObservable: Observable<String?>,
          model: ModelProtocol) {
-        let event = Observable
+        Observable
             .combineLatest(idTextObservable, passwordTextObservable)
             .skip(1)
-            .flatMap { idText, passwordText -> Observable<Event<Void>> in
-                return model
-                    .validate(idText: idText, passwordText: passwordText)
-                    .materialize()
-            }
-            .share()
+            .subscribe(onNext: {(idText: String?, passwordText: String?) -> Void in
+                model.validate(idText: idText, passwordText: passwordText)
+            })
+            .disposed(by: disposeBag)
 
-        self.validationText = event
-            .flatMap { event -> Observable<String> in
-                switch event {
-                case .next:
-                    return .just("OK!!!")
-                case let .error(error as ModelError):
-                    return .just(error.errorText)
-                case .error, .completed:
-                    return .empty()
-                }
-            }
+        self.validationText = model.validationResult
+            .map { result -> String in result.validationResultText }
             .startWith("IDとPasswordを入力してください。")
 
-
-        self.loadLabelColor = event
-            .flatMap { event -> Observable<UIColor> in
-                switch event {
-                case .next:
-                    return .just(.green)
-                case .error:
-                    return .just(.red)
-                case .completed:
-                    return .empty()
-                }
-        }
+        self.loadLabelColor = model.validationResult
+            .map { result -> UIColor in result.validationResultColor }
     }
 }
 
-extension ModelError {
-    fileprivate var errorText: String {
+extension ValidationResult {
+    fileprivate var validationResultText: String {
         switch self {
+        case .valid:
+            return "OK!!!"
         case .invalidIdAndPassword:
             return "IDとPasswordが未入力です。"
         case .invalidId:
             return "IDが未入力です。"
         case .invalidPassword:
             return "Passwordが未入力です。"
+        }
+    }
+    fileprivate var validationResultColor: UIColor {
+        switch self {
+            case .valid:
+                return .green
+            default:
+                return .red
         }
     }
 }
